e12e022b7c8d0ec59e34dc4f81703954ec773b5f
Sean Washington <hello@seanwash.com>
2015-07-31 07:08:57 -0700

    Remove base16 builder files

--- base16-material/output/guake/base16-material.dark.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Base16 material - Guake Terminal color scheme install script
-# Sean Washington (http://seanwash.com)
-
-
-gconftool-2 -s -t string /apps/guake/style/background/color "#262632323838"
-gconftool-2 -s -t string /apps/guake/style/font/color "#ececefefefef"
-gconftool-2 -s -t string /apps/guake/style/font/palette "#262632323838:#cccc66666666:#b5b5bdbd6868:#f0f0c6c67474:#8181a2a2bebe:#b2b29494bbbb:#8a8abebeb7b7:#ececefefefef:#707078788080:#cccc66666666:#b5b5bdbd6868:#f0f0c6c67474:#8181a2a2bebe:#b2b29494bbbb:#8a8abebeb7b7:#ffffffffffff"
--- base16-material/templates/mobaxterm/README.md
+++
@@ -1,1 +0,0 @@
-Note: The contents of the mobaxterm base16 output file goes inside MobaXterm.ini, under the color section
